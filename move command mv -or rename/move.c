c145b5773fbd3f65c73ceb97ca1ab955763813c7
vijay madipally <vijay27goud@gmail.com>
2018-09-06 21:47:29 +0530

    Create move.c

---
+++ move command mv -or rename/move.c
@@ -0,0 +1,35 @@
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+  int src; //source file
+  int dst; //destination file
+  int n; // tracking variable
+  char buf[1];
+  if(argc != 3){
+    printf("usage ./a.out sourceFileName destinationFileName\n");
+    exit(1);
+  }
+
+  src = open(argv[1],O_RDONLY);
+  if(src == -1){
+    perror("can't find the source file");
+    exit(1);
+  }
+
+  dst = creat(argv[2],0777);
+  dst = open(argv[2],O_WRONLY);
+  if(dst == -1){
+    perror("can't create or open destination file");
+    exit(1);
+  }
+
+  while((n=read(src,buf,1))>0){
+    write(dst,buf,1);
+  }
+  close(src);
+  remove(argv[1]);
+  close(dst);
+}
